e99b91a129a103007700ad095c839be4351307d7
xuechun-fan <52503118+xuechun-fan@users.noreply.github.com>
2021-04-19 22:16:17 +0800

    a

---
+++ SpringBoot学习/demo/src/main/java/com/fxc/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.fxc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//  程序的主入口
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
