25f6ec9b92732da561d6f00a93bdc2614ae77202
XuechunFan <1570285527@qq.com>
2021-08-29 16:02:01 +0800

    update
    update

--- SpringBoot学习/springboot-08-shiro/shiro-springboot/src/main/java/com/fxc/config/ShiroConfig.java
+++ SpringBoot学习/springboot-08-shiro/shiro-springboot/src/main/java/com/fxc/config/ShiroConfig.java
@@ -33,8 +33,8 @@ public class ShiroConfig {
         filterMap.put("/user/add", "authc");
         filterMap.put("/user/update", "authc");
         //  授权
-        filterMap.put("/user/add", "perms[user:add]");
-        filterMap.put("/user/update", "perms[user:update]");
+        filterMap.put("/user/add", "perms[admin]");
+        filterMap.put("/user/update", "perms[customer]");
 
         bean.setFilterChainDefinitionMap(filterMap);
 
