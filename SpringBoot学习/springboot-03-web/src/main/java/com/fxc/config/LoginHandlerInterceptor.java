0c1fdf13d10cb8f29b88ae705020be39be714ff6
XuechunFan <1570285527@qq.com>
2021-12-06 14:25:48 +0800

    aaa

--- SpringBoot学习/springboot-03-web/src/main/java/com/fxc/config/LoginHandlerInterceptor.java
+++ SpringBoot学习/springboot-03-web/src/main/java/com/fxc/config/LoginHandlerInterceptor.java
@@ -1,6 +1,5 @@
 package com.fxc.config;
 
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginHandlerInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
         //  登陆成功后，应该有用户的session
         System.out.println("执行了Handler。。。。");
         String requestURL = request.getRequestURI();
